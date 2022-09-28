5cfc3591d1928554c19762e7dd00ffb63dae1777
AvinashKrupa <skavinash49@gmail.com>
2022-09-28 05:52:23 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,3 @@
+  GNU nano 6.2                                                       test.sh                                                                
+#!/bin/bash
+aws s3 cp /root/test/*  s3://devopsavinash
